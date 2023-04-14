6eabee96e7b911927d2e9d345426b67042981d66
AdnanAzem <adnan.azem.250@gmail.com>
2023-04-14 12:04:18 +0300

    init

---
+++ sources/Fraction.cpp
@@ -0,0 +1,10 @@
+#include "Fraction.hpp"
+
+namespace ariel{
+
+    Fraction::Fraction(){}
+
+    Fraction::Fraction(int num1, int num2): numerator(num1), denominator(num2){};
+
+    Fraction::~Fraction(){}
+}
---
+++ sources/Fraction.hpp
@@ -0,0 +1,25 @@
+
+
+namespace ariel{
+
+    class Fraction
+    {
+    private:
+        int numerator;
+        int denominator;
+
+    public:
+        Fraction(/* args */);
+        Fraction(int , int);
+        ~Fraction();
+    };
+    
+    Fraction::Fraction(/* args */)
+    {
+    }
+    
+    Fraction::~Fraction()
+    {
+    }
+    
+}
